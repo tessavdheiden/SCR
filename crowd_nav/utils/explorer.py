a8b7dc2a32226467b747918e861796b625395125
Tessa van der Heiden <tessavdheiden@gmail.com>
2019-12-18 16:46:44 +0100

    class of resultstat modified

--- crowd_nav/utils/explorer.py
+++ crowd_nav/utils/explorer.py
@@ -6,22 +6,68 @@ from numpy.linalg import norm
 
 from crowd_sim.envs.utils.info import *
 
-class Success(ReachGoal):
-    def __init__(self, speed):
-        super().__init__()
-        self.path_goal = None
-        self.human_duration = None
-        self.speed = speed #np.array([])
-        self.acceleration = np.array([])
-        self.jerk = np.array([])
 
-
-class ResultStat:
-    def __init__(self, duration: float, info: Info, cumalative_rewards: float, epoch: int):
+class ResultStat(object):
+    def __init__(self, duration: float, cumulative_rewards: float, epoch: int):
         self.duration = duration
-        self.info = info
-        self.cumalative_rewards = cumalative_rewards
+        self.cumulative_rewards = cumulative_rewards
         self.epoch = epoch
+
+
+class RSSuccess(ResultStat):
+    def __init__(self, duration: float, cumulative_rewards: float, epoch: int, speed, delta_t: float):
+        super().__init__(duration, cumulative_rewards, epoch)
+        self.duration = duration
+        self.cumulative_rewards = cumulative_rewards
+        self.epoch = epoch
+        self.speed = speed
+        self.delta_t = delta_t
+        self.avg_human_time = None
+
+    @property
+    def acceleration(self):
+        return np.diff(self.speed) / self.delta_t
+
+    @property
+    def jerk(self):
+        return np.diff(self.acceleration) / self.delta_t
+
+    def human_times(self, humans, global_time):
+        self.avg_human_time = global_time + sum([norm(np.array(human.get_position()) - np.array(human.get_goal_position()), 2) / human.v_pref for human in humans]) / len(humans)
+
+
+class RSCollision(ResultStat):
+    def __init__(self, duration: float, cumulative_rewards: float, epoch: int):
+        super().__init__(duration, cumulative_rewards, epoch)
+        self.duration = duration
+        self.cumulative_rewards = cumulative_rewards
+        self.epoch = epoch
+
+
+class RSTimeOut(ResultStat):
+    def __init__(self, duration: float, cumulative_rewards: float, epoch: int):
+        super().__init__(duration, cumulative_rewards, epoch)
+        self.duration = duration
+        self.cumulative_rewards = cumulative_rewards
+        self.epoch = epoch
+
+
+class TestData(object):
+    def __init__(self, phase):
+        self.phase = phase
+        self.results = []
+
+    def push(self, result: ResultStat, humans: list, global_time: float):
+        if isinstance(result, RSSuccess):
+            if self.phase in ['val', 'test']:
+                result.human_times(global_time=global_time, humans=humans)
+                self.results.append(result)
+            else:
+                self.results.append(result)
+
+    @property
+    def size(self):
+        return len(self.results)
 
 
 class Explorer(object):
@@ -41,7 +87,8 @@ class Explorer(object):
     def run_k_episodes(self, k, phase, update_memory=False, imitation_learning=False, episode=None,
                        print_failure=False):
         self.robot.policy.set_phase(phase)
-        X = []
+
+        X = TestData(phase)
         too_close = 0
         min_dist = []
         for i in range(k):
@@ -68,11 +115,11 @@ class Explorer(object):
 
             if isinstance(info, ReachGoal):
                 speed = np.asarray([norm(np.array([action.vx, action.vy]), 2) for action in actions])
-                x = ResultStat(self.env.global_time, Success(speed), cumalative_rewards, i)
+                x = RSSuccess(self.env.global_time, cumalative_rewards, i, speed, self.robot.time_step)
             elif isinstance(info, Collision):
-                x = ResultStat(self.env.global_time, info, cumalative_rewards, i)
+                x = RSCollision(self.env.global_time, cumalative_rewards, i)
             elif isinstance(info, Timeout):
-                x = ResultStat(self.env.time_limit, info, cumalative_rewards, i)
+                x = RSTimeOut(self.env.time_limit, cumalative_rewards, i)
             else:
                 raise ValueError('Invalid end signal from environment')
 
@@ -81,37 +128,30 @@ class Explorer(object):
                     # only add positive(success) or negative(collision) experience in experience set
                     self.update_memory(states, human_states, rewards, imitation_learning)
 
+            X.push(x, global_time=self.env.global_time, humans=self.env.humans)
 
-            if phase in ['val', 'test']:
-                if isinstance(x.info, Success):
-                    x.info.human_duration = self.env.global_time + sum([norm(np.array(human.get_position()) - np.array(human.get_goal_position()), 2) / human.v_pref for human in self.env.humans]) / len(self.env.humans)
-                    x.info.acceleration = np.diff(x.info.speed) / self.robot.time_step
-                    x.info.jerk = np.diff(x.info.acceleration) / self.robot.time_step
+        success_cases = [x.epoch for x in X.results if isinstance(x, RSSuccess)] #reduce(lambda a, _: a + 1, (x for x in test_results if isinstance(x, RSSuccess)), 0)
+        collision_cases = [x.epoch for x in X.results if isinstance(x, RSCollision)]
+        avg_nav_time = sum([x.duration for x in X.results if isinstance(x, RSSuccess)]) / len(success_cases) if len(success_cases) else 0
 
-            X.append(x)
-
-
-        success_cases = [x.epoch for x in X if isinstance(x.info, Success)]
-        collision_cases = [x.epoch for x in X if isinstance(x.info, Collision)]
-        avg_nav_time = sum([x.duration for x in X if isinstance(x.info, Success)]) / len(success_cases)
-        avg_nav_length = sum([vel * self.robot.time_step for vel in x.info.speed for x in X if isinstance(x.info, Success)]) / len(success_cases)
-        avg_cumalative_rewards = average([x.cumalative_rewards for x in X])
+        avg_nav_length = sum([vel * self.robot.time_step for vel in x.speed for x in X.results if isinstance(x, RSSuccess)]) / len(success_cases) if len(success_cases) else 0
+        avg_cumulative_rewards = average([x.cumulative_rewards for x in X.results])
 
         extra_info = '' if episode is None else 'in episode {} '.format(episode)
         logging.info('{:<5} {}has success rate: {:.2f}, collision rate: {:.2f}, nav time: {:.2f}, nav length: {:.2f}, total reward: {:.4f}'.
                      format(phase.upper(), extra_info, len(success_cases) / k, len(collision_cases) / k, avg_nav_time, avg_nav_length,
-                            avg_cumalative_rewards))
+                            avg_cumulative_rewards))
 
         if phase in ['val', 'test']:
-            total_time = sum([x.duration for x in X]) * self.robot.time_step
-            avg_jerk = average([abs(x.info.jerk).mean() for x in X if isinstance(x.info, Success)])
-            avg_human_times = average([x.info.human_duration for x in X if isinstance(x.info, Success)])
+            total_time = sum([x.duration for x in X.results]) * self.robot.time_step
+            avg_jerk = average([x.jerk.mean() for x in X.results if isinstance(x, RSSuccess)])
+            avg_human_times = average([x.avg_human_time for x in X.results if isinstance(x, RSSuccess)])
             logging.info('Frequency of being in danger: {:.2f} and average min separate distance in danger: {:.2f}, '
                          'jerk: {:.4f}, human nav time: {:.2f}'.format(
                          too_close / total_time, average(min_dist), avg_jerk, avg_human_times))
 
         if print_failure:
-            timeout_cases = [x.epoch for x in X if isinstance(x.info, Timeout)]
+            timeout_cases = [x.epoch for x in X.results if isinstance(x.info, Timeout)]
             logging.info('Collision cases: ' + ' '.join([str(x) for x in collision_cases]))
             logging.info('Timeout cases: ' + ' '.join([str(x) for x in timeout_cases]))
 
