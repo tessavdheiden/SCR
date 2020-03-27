81ea5faefa5d67d573ed74e35ebeb70c6892f808
tessavdheiden <tessavdheiden@gmail.com>
2020-03-27 16:02:53 +0100

    test for trans

--- crowd_nav/utils/transformations.py
+++ crowd_nav/utils/transformations.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-def build_occupancy_map(human, other_humans):
+def build_occupancy_map(human, other_humans, cell_num, cell_size, om_channel_size):
     """
     :param human_states:
     :param robot_state
-    :return: tensor of shape (# human - 1, self.cell_num ** 2)
+    :return: tensor of shape (# human - 1, cell_num ** 2)
     """
     other_px = other_humans[:, 0] - human.px
     other_py = other_humans[:, 1] - human.py
@@ -17,15 +17,15 @@ def build_occupancy_map(human, other_humans):
     other_py = np.sin(rotation) * distance
 
     # compute indices of humans in the grid
-    other_x_index = np.floor(other_px / self.cell_size + self.cell_num / 2)
-    other_y_index = np.floor(other_py / self.cell_size + self.cell_num / 2)
+    other_x_index = np.floor(other_px / cell_size + cell_num / 2)
+    other_y_index = np.floor(other_py / cell_size + cell_num / 2)
     other_x_index[other_x_index < 0] = float('-inf')
-    other_x_index[other_x_index >= self.cell_num] = float('-inf')
+    other_x_index[other_x_index >= cell_num] = float('-inf')
     other_y_index[other_y_index < 0] = float('-inf')
-    other_y_index[other_y_index >= self.cell_num] = float('-inf')
-    grid_indices = self.cell_num * other_y_index + other_x_index
-    occupancy_map = np.isin(range(self.cell_num ** 2), grid_indices)
-    if self.om_channel_size == 1:
+    other_y_index[other_y_index >= cell_num] = float('-inf')
+    grid_indices = cell_num * other_y_index + other_x_index
+    occupancy_map = np.isin(range(cell_num ** 2), grid_indices)
+    if om_channel_size == 1:
         return occupancy_map.astype(int)
     else:
         # calculate relative velocity for other agents
@@ -34,18 +34,22 @@ def build_occupancy_map(human, other_humans):
         speed = np.linalg.norm(other_humans[:, 2:4], axis=1)
         other_vx = np.cos(rotation) * speed
         other_vy = np.sin(rotation) * speed
-        dm = [list() for _ in range(self.cell_num ** 2 * self.om_channel_size)]
+        dm = [list() for _ in range(cell_num ** 2 * om_channel_size)]
         for i, index in np.ndenumerate(grid_indices):
-            if index in range(self.cell_num ** 2):
-                if self.om_channel_size == 2:
+            if index in range(cell_num ** 2):
+                if om_channel_size == 2:
                     dm[2 * int(index)].append(other_vx[i])
                     dm[2 * int(index) + 1].append(other_vy[i])
-                elif self.om_channel_size == 3:
+                elif om_channel_size == 3:
                     dm[int(index)].append(1)
-                    dm[int(index) + self.cell_num ** 2].append(other_vx[i])
-                    dm[int(index) + self.cell_num ** 2 * 2].append(other_vy[i])
+                    dm[int(index) + cell_num ** 2].append(other_vx[i])
+                    dm[int(index) + cell_num ** 2 * 2].append(other_vy[i])
                 else:
                     raise NotImplementedError
         for i, cell in enumerate(dm):
             dm[i] = sum(dm[i]) / len(dm[i]) if len(dm[i]) != 0 else 0
         return [dm]
+
+
+def propagate_occupancy_map(occupancy_map, action):
+    pass
--- crowd_nav/utils/transformations_test.py
+++ crowd_nav/utils/transformations_test.py
@@ -1,13 +1,19 @@
 import unittest
 
+import numpy as np
 from crowd_nav.utils.transformations import build_occupancy_map
+from crowd_sim.envs.utils.human import Human
 
 
 class TransformationsTest(unittest.TestCase):
     def test_build_occupancy_map(self):
-        human = []
-        other_humans = []
-        self.assertEqual(build_occupancy_map(human, other_humans), [])
+        cell_num = 4
+        cell_size = 1
+        om_channel_size = 1
+        human = Human()
+        human.set(px=0, py=0, vx=0, vy=0, gx=0, gy=0, theta=0)
+        other_humans = np.zeros((1, 4))
+        self.assertEqual(build_occupancy_map(human, other_humans, cell_num, cell_size, om_channel_size), np.zeros((cell_num ** 2 * om_channel_size)))
 
 if __name__ == "__main__":
     unittest.main()
