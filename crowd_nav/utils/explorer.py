fe3c3472d53caac9d61aecd424d1e9824c88cbe2
tessavdheiden <tessavdheiden@gmail.com>
2020-05-03 08:56:17 +0200

    reduced string clutter

--- crowd_nav/utils/explorer.py
+++ crowd_nav/utils/explorer.py
@@ -50,13 +50,9 @@ class Explorer(object):
         self.robot.policy.set_phase(phase)
 
         too_close = 0
-        min_dist = []
-
         ob = self.env.reset(phase)
         done = False
-        rewards = []
-        actions = []
-
+        min_dist, rewards, actions = ([] for _ in range(3))
         while not done:
             action = self.robot.act(ob)
             ob, reward, done, info = self.env.step(action)
@@ -80,11 +76,11 @@ class Explorer(object):
 
         if isinstance(info, ReachGoal):
             human_times = average(self.env.get_human_times())
-            extra_info = 'success, avg human time: {:.2f}'.format(human_times)
+            extra_info = f'success, avg human time: {human_times:.2f}'
         elif isinstance(info, Collision):
-            extra_info = 'collision'
+            extra_info = f'collision'
         elif isinstance(info, Timeout):
-            extra_info = 'time out'
+            extra_info = f'time out'
         else:
             raise ValueError('Invalid end signal from environment')
 
@@ -92,41 +88,29 @@ class Explorer(object):
         path_length = get_path_length(speed, self.robot.time_step)
         jerk = sum(get_jerk(get_acceleration(speed, self.robot.time_step), self.robot.time_step)) / len(actions)
 
-        logging.info('{:<5} has {}, nav time: {:.2f}, nav path length: {:.2f}, jerk: {:.2f}, total reward: {:.4f}'.
-                     format(phase.upper(), extra_info,  self.env.global_time, path_length, jerk, avg_rewards))
+        logging.info(f'{phase.upper():<5} has {extra_info}, nav time: {self.env.global_time:.2f},'
+                     f' nav path length: {path_length:.2f}, jerk: {jerk:.2f}, total reward: {avg_rewards:.4f}')
 
-        logging.info('Frequency of being in danger: %.2f and average min separate distance in danger: %.2f',
-                     too_close / self.env.global_time, average(min_dist))
+        logging.info(f'Frequency of being in danger: {too_close / self.env.global_time:.2f},'
+                     f' and average min separate distance in danger: {average(min_dist):.2f}')
 
 
-    def run_k_episodes(self, k, phase, update_memory=False, imitation_learning=False, episode=None,
-                       print_failure=False):
+    def run_k_episodes(self, k, phase, update_memory=False, imitation_learning=False, episode=None, print_failure=False):
         self.robot.policy.set_phase(phase)
-        success_times = []
-        collision_times = []
-        timeout_times = []
-        success = 0
-        collision = 0
-        timeout = 0
-        too_close = 0
-        min_dist = []
-        cumulative_rewards = []
-        collision_cases = []
-        timeout_cases = []
+
+        success_times, collision_times, timeout_times, min_dist, cumulative_rewards, collision_cases, timeout_cases = ([] for _ in range(7))
+        success, collision, timeout, too_close = 0, 0, 0, 0
+
         for i in range(k):
             ob = self.env.reset(phase)
             done = False
-            joined_states = []
-            actions = []
-            rewards = []
-            states = []
+            joined_states, rewards, states = ([] for _ in range(3))
             while not done:
                 action = self.robot.act(ob)
                 ob, reward, done, info = self.env.step(action)
 
                 joined_states.append(self.robot.policy.last_state)
                 states.append(ob + [self.robot.get_observable_state()])
-                actions.append(action)
                 rewards.append(reward)
 
                 if isinstance(info, Danger):
@@ -161,13 +145,14 @@ class Explorer(object):
         avg_nav_time = sum(success_times) / len(success_times) if success_times else self.env.time_limit
 
         extra_info = '' if episode is None else f'in episode {episode} '
-        logging.info('{:<5} {}has success rate: {:.2f}, collision rate: {:.2f}, nav time: {:.2f}, total reward: {:.4f}'.
-                     format(phase.upper(), extra_info, success_rate, collision_rate, avg_nav_time,
-                            average(cumulative_rewards)))
+        logging.info(f'{phase.upper():<5} {extra_info}has success rate: {success_rate:.2f}, '
+                     f'collision rate: {collision_rate:.2f}, nav time: {avg_nav_time:.2f}, '
+                     f'total reward: {average(cumulative_rewards):.4f}')
+
         if phase in ['val', 'test']:
             total_time = sum(success_times + collision_times + timeout_times) * self.robot.time_step
-            logging.info('Frequency of being in danger: %.2f and average min separate distance in danger: %.2f',
-                         too_close / total_time, average(min_dist))
+            logging.info(f'Frequency of being in danger: {too_close / total_time:.2f},'
+                         f' average min separate distance in danger: {average(min_dist):.2f}')
 
         if print_failure:
             logging.info('Collision cases: ' + ' '.join([str(x) for x in collision_cases]))
